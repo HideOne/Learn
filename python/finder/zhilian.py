17e668d879c3042b09206209c5e617f89efa9edf
HideOne <844085696@qq.com>
2019-10-24 14:03:06 +0800

    flu

--- python/finder/zhilian.py
+++ python/finder/zhilian.py
@@ -1,15 +1,33 @@
-# ending:utf-8
+# coding:utf-8
 import re
+import urllib
 
-zhiLianRe = '<div class="totalnum inlineblock" data-a-29bcac4f="">为您优选<span style="color:#5183FF;" data-a-29bcac4f="">1186</span>个放心职位</div>'
+import selenium
+import selenium.webdriver as webdriver
 
-reStr = '<div class="totalnum inlineblock" data-a-29bcac4f="">为您优选<span style="color:#5183FF;" data-a-29bcac4f="">(\\d+)</span>个放心职位</div>'
+zhiLianRe = """<div class="totalnum inlineblock" data-a-29bcac4f="">为您优选<span style="color:#5183FF;" data-a-29bcac4f="">1186</span>个放心职位</div>"""
+
+reStr     = """为您优选<span style="color:#5183FF;" data-a-29bcac4f="">(\d+)</span>个放心职位"""
+
+jobStr = """ <i>(\d+)</i> """
 
 
 def main():
-    regex = re.compile(zhiLianRe, re.IGNORECASE)
-    ret = regex.findall(reStr)
+    # regex = re.compile(reStr, re.IGNORECASE)
+
+    # # url1 = 'http://zhaopin.baidu.com/quanzhi'
+    # ret = regex.findall(zhiLianRe, re.IGNORECASE)
+    # print ret
+    url = 'https://cd.58.com/lvyouguwen/?utm_source=market&ClickID=2'
+    driver = webdriver.Chrome()
+    driver.get(url)
+    data = driver.page_source
+    print data
+    regex = re.compile(jobStr, re.IGNORECASE)
+    ret = regex.findall(data)
+
     print(ret)
+    print(ret[0])
     pass
 
 
